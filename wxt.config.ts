08645d18b1f4dc3405502e51bd9a017ffa19e7b2
Minsoo Choo <minsoochoo0122@proton.me>
2025-09-07 19:24:32 -0400

    feat: prepare beta release

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,19 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	srcDir: 'src',
-	modules: ['@wxt-dev/module-svelte'],
+	modules: ['@wxt-dev/module-svelte', '@wxt-dev/auto-icons'],
+	manifest: {
+		name: 'Quest Schedule Exporter',
+		description: 'Since uWaterloo Quest does not support exporting your schedule to an iCalendar file and the UI sucks, this extension allows you to export your schedule to an iCalendar file.',
+		category: 'productivity',
+		lang: 'en',
+		icons: {
+			'16': '/icons/16.png',
+			'32': '/icons/32.png',
+			'48': '/icons/48.png',
+			'128': '/icons/128.png'
+		}
+	},
 	vite: () => ({
 		plugins: [tailwindcss()]
 	})
